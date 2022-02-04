e75738ab69f506b0d3877ed3dfaddc5cd22032aa
Cristian Souza <cristiansouza704@gmail.com>
2022-02-04 13:50:20 -0300

    formulario-footer

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,8 @@
-function botaoMenu () {
-    constructor
-}
+var menunav = document.getElementById('navbar-itens');
+var botao = document.getElementById('btn-menu');
+var botaoFechar = document.getElementById('btn-menu').children[0];
+
+botao.addEventListener('click', () =>{
+    menunav.classList.toggle('active');
+} 
+)

3edbce43cd1a24a05ba6ca668c1dc677c8e00524
Cristian Souza <cristiansouza704@gmail.com>
2022-02-06 20:19:39 -0300

    atualização

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,46 @@
-var menunav = document.getElementById('navbar-itens');
-var botao = document.getElementById('btn-menu');
-var botaoFechar = document.getElementById('btn-menu').children[0];
+let nome = document.getElementById('idNome');
+let labelNome = document.querySelector('#lNome')
+let valNome = false;
 
-botao.addEventListener('click', () =>{
-    menunav.classList.toggle('active');
-} 
-)
+let cpf = document.querySelector("#idCpf");
+let labelCpf = document.querySelector('#lCpf')
+let valCpf = false;
+
+let telefone = document.querySelector(".idTel");
+let valTelefone = false;
+
+let email = document.querySelector(".idEmail");
+let valEmail = false;
+
+let cep = document.querySelector(".idCep");
+let valCep = false;
+
+let bairro = document.querySelector(".idBairro");
+let valBairro = false;
+
+let cidade = document.querySelector(".idCidade");
+let valCidade = false;
+
+let estado = document.querySelector(".idEstado");
+let valEstado = false;
+
+nome.addEventListener('keyup', () =>{
+    if (nome.value.length <= 2 ) {
+        alert('Diigite');
+    } else {
+        
+    }
+})
+cpf.addEventListener('keyup', () =>{
+    if (cpf.value.length <= 2 ) {
+        labelCpf.setAttribute('style', 'color: red')
+    } else {
+        
+    }
+})
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    checkinputs() 
+})
